5b6f246e84fda85bcd8c17f2328cd2d470b81318
Steve Pond <sapond@gmail.com>
2025-06-14 18:41:25 -0500

    services running

--- packages/api/src/temporal/worker.ts
+++ packages/api/src/temporal/worker.ts
@@ -1,13 +1,20 @@
-import { Worker } from '@temporalio/worker';
-import * as activities from './activities.js';
-import * as workflows from './workflows.js';
+import { Worker, NativeConnection, DefaultLogger } from "@temporalio/worker";
+import * as activities from "./activities.js";
+import * as workflows from "./workflows.js";
 
 export const startWorker = async () => {
+  const connection = await NativeConnection.connect({
+    address: "temporal:7233",
+  });
+
+  const logger = new DefaultLogger("WARN");
+
   const worker = await Worker.create({
-    workflowsPath: new URL('./workflows.js', import.meta.url).pathname,
+    workflowsPath: new URL("./workflows.js", import.meta.url).pathname,
     activities,
-    taskQueue: 'podcast-digest',
+    taskQueue: "podcast-digest",
+    connection,
   });
 
   await worker.run();
-}; 
+};
